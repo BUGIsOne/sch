aedfd31e924a4fe8ee73a8954085cfc394819073
bugisone <937459807@qq.com>
2019-09-24 20:36:28 +0800

    完成命令-c -d字母统计

--- WT.cpp
+++ WT.cpp
@@ -6,50 +6,104 @@
 #include<string>
 #include<algorithm>
 #include<cstring>
+#include<io.h>
 
 using namespace std;
 
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 // int main()
 {
   // 打开文件
   ifstream order(argv[1]);
   ifstream input(argv[2]);
+  ofstream output("./output.txt");
 
   string str_order = "";
   int argv1_len = strlen(argv[1]);
   for(int i=0; i<argv1_len; i++)
     str_order += (argv[1][i]);
 
-  // ifstream input("./test.txt");
+  
 
   string s;
   int total_ch = 0;
   map <char, size_t> map1;
   map <char, size_t>::iterator it_map;
 
-  // if(output && input)
-  if(input)
+  string str_line;
+  //argv[1].length is right and input is not none
+	if(str_order.length() == 2)
   {
-    while(getline(input,s))
-    {
-      int len = s.length();
-      transform(s.begin(),s.end(),s.begin(), ::tolower);
-      for(int i=0; i<len; i++)
-      {
-        char ch = s[i];
-        if(isalpha(ch))
-          ++map1[ch];
-      }
-    }
-  }
+		cout<<str_order<<endl<<endl;
+  	if(str_order == "-c")
+  	{
+	    while(getline(input,s))
+	    {
+	      int len = s.length();
+	      transform(s.begin(),s.end(),s.begin(), ::tolower);
+	      for(int i=0; i<len; i++)
+	      {
+	        char ch = s[i];
+	        if(isalpha(ch))
+	          ++map1[ch];
+	      }
+	    }
+	  		
+	    for(it_map=map1.begin(); it_map != map1.end(); it_map++)
+	    {
+	  		output<<it_map->first<<" "<<it_map->second<<endl;	
+			}
+		}
+	
+  	else if(str_order == "-d")
+  	{
+			_finddata_t file;
+			long lf;
+			char dir_file[50]="";
+			strcat(dir_file, argv[2]);
+			strcat(dir_file, "\\*.txt");
+			int k=1;
+			long HANDLE;
+			k = HANDLE = _findfirst(dir_file, &file);
+			// k = HANDLE = _findfirst(dir_file, &file);
+			cout<<dir_file<<endl<<endl;
+			//read every txt in the dir
+			while( k != -1 )
+			{ 					
+				output << file.name << endl;	
+				char str_fn[50] ="";
+				strcat(str_fn, argv[2]);
+				strcat(str_fn, "\\");
+				strcat(str_fn, file.name);
+				ifstream fn(str_fn);
+				while(getline(fn,s))
+		    {
+		    	//cout<<"succ"<<endl;
+		      int len = s.length();
+		      transform(s.begin(),s.end(),s.begin(), ::tolower);
+		      for(int i=0; i<len; i++)
+		      {
+		        char ch = s[i];
+		        if(isalpha(ch))
+		          ++map1[ch];
+		      }
+		    }
+		  				
+		    for(it_map=map1.begin(); it_map != map1.end(); it_map++)
+		    {
+		    	//cout<<it_map->first<<" "<<it_map->second<<endl;
+		  		output<<it_map->first<<" "<<it_map->second<<endl;	
+				}
+				output<<endl<<endl;
+				k = _findnext( HANDLE, &file );	
+			}
+			_findclose( HANDLE );
 
-
-  
-  if(str_order.length() == 2 && str_order=="-c")
-    for(it_map=map1.begin(); it_map != map1.end(); it_map++)
-      cout<<it_map->first<<" "<<it_map->second<<endl;
-  
+		}
+ }
+    
+	input.close();
+	output.close();
   // cout<<str_order;
   // getchar();
   return 0;
